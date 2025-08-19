c5755a6c1be1219922712e0c29a72883dbc7e67a
Varnika <vvarnika05@gmail.com>
2025-08-19 17:52:45 +0530

    Simple Sieve

---
+++ SimpleSieve.java
@@ -0,0 +1,25 @@
+public class SimpleSieve {
+    public static void simpleSieve(int limit) {
+        boolean[] prime = new boolean[limit + 1];
+        for (int i = 2; i <= limit; i++) {
+            prime[i] = true; // they are false by default
+        }
+        for (int p = 2; p * p <= limit; p++) {
+            if (prime[p] == true) {
+                for (int i = p * p; i <= limit; i += p) {
+                    prime[i] = false;
+                }
+            }
+        }
+        for (int i = 2; i <= limit; i++) {
+            if (prime[i] == true) {
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        simpleSieve(50);
+    }
+}
